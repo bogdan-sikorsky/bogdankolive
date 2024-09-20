79289aee14c9f1cbc73f0554b33121c34c395634
bogdan <bogdan.sikorsky.dev@gmail.com>
2024-09-20 12:07:28 +0300

    [ref] unexpandable navigation, fixed links in the sidebar, config.toml, links in proj page

--- app/helpers/toc.py
+++ app/helpers/toc.py
@@ -1,6 +1,16 @@
 import re
 
 
+# Generate the table of contents based on the extracted biggest headers
+def generate_toc(markdown_text):
+    headers = extract_biggest_headers(markdown_text)
+    toc = '<div class="toc">'
+    for title, header_id in headers:
+        toc += f'<a href="#{header_id}">{title}</a>'
+    toc += '</div>'
+    return toc
+
+
 # Extract only the biggest (level 1) headers from markdown
 def extract_biggest_headers(markdown_text):
     headers = []
@@ -12,22 +22,3 @@ def extract_biggest_headers(markdown_text):
             header_id = re.sub(r'\s+', '-', title.lower())  # Generate an anchor id
             headers.append((title, header_id))
     return headers
-
-
-# Generate the table of contents based on the extracted biggest headers
-def generate_toc(markdown_text):
-    headers = extract_biggest_headers(markdown_text)
-    toc = '<div class="toc">'
-    for title, header_id in headers:
-        toc += f'<a href="#{header_id}">{title}</a>'
-    toc += '</div>'
-    return toc
-
-
-# Insert markdown content with anchors
-def generate_content_with_anchors(markdown_text, headers):
-    content = markdown_text
-    for title, header_id in headers:
-        anchor_tag = f'<a id="{header_id}"></a>'
-        content = content.replace(f'# {title}', f'# {title}{anchor_tag}')
-    return content
